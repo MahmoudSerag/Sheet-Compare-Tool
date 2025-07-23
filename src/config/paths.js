8b6deb1f546e7684846512bd3381b1c5ca9a45b4
MahmoudSerag <sragmahmoud4@gmail.com>
2025-07-23 23:09:19 +0300

    Add path configuration for source and system transaction sheets

---
+++ src/config/paths.js
@@ -0,0 +1,6 @@
+const path = require("path");
+
+module.exports = {
+  sheet1Path: path.resolve(__dirname, "../../data/source_transactions.csv"),
+  sheet2Path: path.resolve(__dirname, "../../data/system_transactions.csv"),
+};
